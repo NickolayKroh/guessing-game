468f12b0275f19f0286449b8b531a783703810d0
Kroh Nickolay <mikolka.del@gmail.com>
2019-10-01 22:21:54 +0300

    task complited

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.answ = Math.ceil((this.min + this.max) / 2);
+        return this.answ;
     }
 
     lower() {
-
+        this.max = this.answ;
     }
 
     greater() {
-
+        this.min = this.answ;
     }
 }
 
